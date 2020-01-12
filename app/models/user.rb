197a013a8c7dd5eddade97d065a75c110087763f
Maikel Chandika <mkdika@gmail.com>
2020-01-12 22:49:18 +0700

    First init

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # disabled: registerable
+  devise :database_authenticatable, :lockable,
+         :recoverable, :rememberable
+
+  validates :email, presence: true
+
+  def show_display_name
+    self.display_name.present? ? self.display_name : self.email
+  end
+
+  def locked?
+    self.locked_at.present?
+  end
+end
